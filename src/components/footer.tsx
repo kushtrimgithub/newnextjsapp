689f68633a623316f42699db80a57d75b4c8b4b0
kushtrimgithub <hamzaj.kushtrim@gmail.com>
2024-01-29 21:35:28 +0100

    new changes

---
+++ src/components/footer.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { createClient } from "@/prismicio";
+
+
+export default async function Footer() {
+  const client = createClient();
+  const call = await client.getSingle("cta");
+  const settings = await client.getSingle("settings");
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link href="/">
+          Logo
+        </Link>
+      </div>
+      <div className="menu">
+        <Link href="/">
+          About
+        </Link>
+        <Link href="/about">
+          About
+        </Link>
+        <Link href="/services">
+          Services
+        </Link>
+        <Link href="/contact">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  )
+}
