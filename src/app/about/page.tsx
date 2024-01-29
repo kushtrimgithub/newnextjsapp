853ddf08b582122957628756ef1cad77348ff724
kushtrimgithub <hamzaj.kushtrim@gmail.com>
2024-01-29 20:59:57 +0100

    new changes

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { createClient } from "@/prismicio";
+
+export default async function About() {
+  
+  return (
+    <div>
+      <h1>This is about</h1>
+    </div>
+  )
+}
