56a9ce278fe1827a07c219371b090f012e1ad9d9
kushtrimgithub <hamzaj.kushtrim@gmail.com>
2024-01-29 21:13:58 +0100

    changes

---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import { createClient } from "@/prismicio";
+
+
+export default async function Header() {
+  const client = createClient();
+  const call = await client.getSingle("cta");
+
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link href="/">
+          Logo
+        </Link>
+      </div>
+      <div className="menu">
+        <Link href="/">
+          About
+        </Link>
+        <Link href="/about">
+          About
+        </Link>
+        <Link href="/services">
+          Services
+        </Link>
+        <Link href="/contact">
+          Contact
+        </Link>
+      </div>
+    </nav>
+  )
+}
