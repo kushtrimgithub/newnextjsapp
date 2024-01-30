e9ba3976254efd3e3ba81aa139e07dc5258b04d1
kushtrimgithub <hamzaj.kushtrim@gmail.com>
2024-01-30 23:37:09 +0100

    changes

--- src/app/blog/[uid]/page.tsx
+++ src/app/blog/[uid]/page.tsx
@@ -4,7 +4,6 @@ import { SliceZone } from "@prismicio/react";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
-import { PostCard } from "@/components/PostCard";
 
 type Params = { uid: string };
 
@@ -27,16 +26,12 @@ export async function generateMetadata({
     .getByUID("blog_post", params.uid)
     .catch(() => notFound());
 
-  return (
-    <div>
-      title: page.data.meta_title,
+  return {
+    title: page.data.meta_title,
     description: page.data.meta_description,
-    <h2 className="font-bold text-xl">
-      {page.data.headline}
-    </h2>
-    </div>
-  );
+  };
 }
+
 
 export async function generateStaticParams() {
   const client = createClient();
