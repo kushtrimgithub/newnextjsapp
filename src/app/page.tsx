579ac3d113eeb5302905ff6bf96ad079cbbfe954
kushtrimgithub <hamzaj.kushtrim@gmail.com>
2024-01-31 09:13:50 +0100

    changes2 git add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,21 @@
-// ./src/app/page.tsx
-
+import { PostCard } from "@/components/PostCard";
 import { Metadata } from "next";
-
 import { SliceZone } from "@prismicio/react";
-import * as prismic from "@prismicio/client";
 
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
-import { PostCard } from "@/components/PostCard";
 
-/**
- * This component renders your homepage.
- *
- * Use Next's generateMetadata function to render page metadata.
- *
- * Use the SliceZone to render the content of the page.
- */
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
-  const home = await client.getSingle("homepage");
+  const page = await client.getSingle("homepage");
 
   return {
-    title: prismic.asText(home.data.title),
-    description: home.data.meta_description,
-    openGraph: {
-      title: home.data.meta_title || undefined,
-      images: [
-        {
-          url: home.data.meta_image.url || "",
-        },
-      ],
-    },
+    title: page.data.meta_title,
+    description: page.data.meta_description,
   };
 }
+
 
 export default async function Index() {
   const client = createClient();
